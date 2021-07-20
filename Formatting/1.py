c5e192ce7facd0c67b10b36141fa005df4b54bb2
Santanu Saha <santanu2539@gmail.com>
2021-07-20 11:22:21 +0530

    Python formatting

---
+++ Formatting/1.py
@@ -0,0 +1,4 @@
+print("The value of x is {} and y is {} and z is {} is".format(10,20,30))
+print("The value of x is {0} and y is {1} and z is {2} is".format(10,20,30)) 
+# 0 means index 0,1 means index i and 2 means index 2
+print("The value of x is {2} and y is {1} and z is {0} is".format(10,20,30))
